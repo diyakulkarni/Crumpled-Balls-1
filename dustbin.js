602c289d643bf0fb10429a9ae18f924804a468a3
diyakulkarni <68670477+diyakulkarni@users.noreply.github.com>
2020-10-06 20:31:40 -0700

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,16 @@
+class Dustbin {
+    constructor(x,y,width,height){
+var options={
+    isStatis:false,
+    restitution:0.3,
+    friction:0.5,
+    density:1.2
+}
+this.body= Bodies.rectangle(x,y,width,height);
+this.width = this.width;
+this.height = this.height;
+World,add(world,this.body);
+    }
+
+    
+}
